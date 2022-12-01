29f36abe9a4441aa27111a9a58b22ac8912bbeb1
Mason <mason.rozumny23@d155.org>
2022-12-01 08:03:35 -0600

    Initial Commit

---
+++ bracketApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  bracketApp
+//
+//  Created by JAMES DURCAN on 12/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var test = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
